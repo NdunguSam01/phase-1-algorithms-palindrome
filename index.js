fa4da9edc21b7ae698d9e1756f5163bd5ce3f9e3
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-18 14:31:18 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(string)
+{
+    string=string.toLowerCase()
+    
+    let reversedString=""
+
+    for (let i = string.length - 1; i >= 0; i--) 
+    {
+        reversedString += string[i];
+    }
+    return reversedString
 }
+
+function isPalindrome(word) 
+{
+    word=word.toLowerCase()
+    const reversedString=reverseString(word)
+
+    let checkPalindrome= (word===reversedString) ? true : false
+
+    return checkPalindrome
+}
+console.log(isPalindrome("Madam"))
 
 /* 
   Add your pseudocode here
+  function reverseString(string)
+  {
+    convert the string to lower case
+    
+    loop over the string to reverse it
+      return the reversed string as the output
+  }
+
+  function isPalindrome(word)
+  {
+    converting the word passed to lower case
+
+    invoking the reverseString function and reversing the string passed by the user
+
+    checking if the reversed string is equal to the passed in string
+      return true or false
+  }
 */
 
 /*
